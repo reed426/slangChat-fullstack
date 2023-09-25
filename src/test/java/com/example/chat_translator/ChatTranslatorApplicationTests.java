d13263a47863a8fb7330b18291184ebae49ca7ba
reed426 <red426@sookmyung.ac.kr>
2023-09-25 23:31:06 +0900

    init

---
+++ src/test/java/com/example/chat_translator/ChatTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
